f15aebf43dcca7e45e7a5dab4e5e44f1d4388b83
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-07-03 16:45:36 -0500

    Add notes to keep in mind before starting the project

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -16,3 +16,12 @@ RSpec.describe Card do
     expect(card.rank).to eq(12)
   end
 end
+
+
+# check out a youtube video on how to play war before beginning the project.
+
+# We're going to get errors and we're going to build our Card class based on the feedback we get from running our tests.
+# Now we're going to make a deck class
+# Iteration 1 is to build our card class and our deck class. The goal is to understand how to build our classes and have them interact. Learn how to call tests.
+# Iteration 2 is to create a new class called Player and a turn class. then we will create new methods for them to interact. 
+# Iteration 3 
