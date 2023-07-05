2811d13d2c2f4de188a76b6b66ad89b3d99707e8
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-07-05 00:31:24 -0500

    Add new lines of code to the project

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,18 +1,16 @@
-require './card.rb'
-
-
 class Deck
     attr_reader :cards
 
     def initialize(cards)
         @cards = cards
     end
-
+    
     def rank_of_card_at(index_position)
-        return cards
+        
+        # require 'pry'; binding.pry
+        @cards[index_position].rank
     end
 
 
 end
 
-p rank_of_card_at()
