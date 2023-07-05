96ccd1f3f673ba26a3842eba10db2ef533b43c96
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-07-05 16:42:20 -0500

    Start building the has lost? function

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,14 @@
 class Player
-    attr_reader :deck
+    attr_reader :name, :deck
 
-    def initialize(deck)
+    def initialize(name, deck)
+        # require 'pry';binding.pry
+        @name = name
         @deck = deck
+    end
+
+    def has_lost?
+        # need to add a contigent for WHEN the game is lost.
+        # the player has no more cards. Use an until, or while method
     end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,6 +5,27 @@ require './lib/player.rb'
 
 describe Player do
     it "exists" do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)    
+        card3 = Card.new(:heart, 'Ace', 14)    
+        cards = [card1, card2, card3]
+
+        deck = Deck.new(cards)
+
+        player = Player.new("Clarisa", deck)
+    end
+
+    it "can determine if the player has lost the game" do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)    
+        card3 = Card.new(:heart, 'Ace', 14)    
+        cards = [card1, card2, card3]
+
+        deck = Deck.new(cards)
+
+        player = Player.new("Clarisa", deck)
+
         
+
     end
 end
