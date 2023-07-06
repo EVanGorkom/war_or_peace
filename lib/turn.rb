1c490185760a827c5505f255bef355f42451b739
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-07-06 09:33:48 -0500

    Add code to the turn file

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,10 +1,9 @@
 class Turn
-    attr_reader :player1, :player2, :spoils_of_war
+    attr_reader :player1, :player2, :spoils_of_war, :turn
 
     def initialize(player1, player2)
         @player1 = player1
         @player2 = player2
-        @turn = basic
     end
 
     def type
