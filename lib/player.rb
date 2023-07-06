8121270b316477a32395265199589532538fde9e
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-07-06 00:16:02 -0500

    Add lines of code for the Player class and modify the has_lost? method

--- lib/player.rb
+++ lib/player.rb
@@ -2,13 +2,16 @@ class Player
     attr_reader :name, :deck
 
     def initialize(name, deck)
-        # require 'pry';binding.pry
         @name = name
         @deck = deck
     end
 
     def has_lost?
-        # need to add a contigent for WHEN the game is lost.
-        # the player has no more cards. Use an until, or while method
+        require 'pry';binding.pry
+        if @cards.length == 0
+            true
+        else
+            false
+        end
     end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -12,7 +12,23 @@ describe Player do
 
         deck = Deck.new(cards)
 
-        player = Player.new("Clarisa", deck)
+        player1 = Player.new("Clarisa", deck)
+
+        expect(player1).to be_an_instance_of(Player)
+    end
+
+    it "has readable attributes" do
+        card1 = Card.new(:diamond, 'Queen', 12)
+        card2 = Card.new(:spade, '3', 3)    
+        card3 = Card.new(:heart, 'Ace', 14)    
+        cards = [card1, card2, card3]
+
+        deck = Deck.new(cards)
+
+        player1 = Player.new("Clarisa", deck)
+
+        expect(player1.name).to eq("Clarisa")
+        expect(player1.deck).to eq(deck)
     end
 
     it "can determine if the player has lost the game" do
@@ -23,9 +39,9 @@ describe Player do
 
         deck = Deck.new(cards)
 
-        player = Player.new("Clarisa", deck)
+        player1 = Player.new("Clarisa", deck)
 
+        expect(player1.has_lost?).to eq(false)
         
-
     end
 end
